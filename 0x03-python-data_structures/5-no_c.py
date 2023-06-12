9bb3935d093e5693b5cbccb19fa6ab28bc73e0f9
INNOXSE <innocentefeakpoyibo@gmail.com>
2023-06-12 05:03:35 -0700

    matrix_int

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
-def no_c(my_string)
-    new_string = "".join(i for i in my_string if i.lower() != "c")
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
     return new_string
