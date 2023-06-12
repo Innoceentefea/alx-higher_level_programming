a0ae584360f27a91cc952684a805e61aa47477ff
INNOXSE <innocentefeakpoyibo@gmail.com>
2023-06-11 19:40:25 -0700

    removing_char

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = ""
-    for i in my_string:
-        if i.lower() != "c":
-            new_string.join(i)
+    new_string = "".join(i for i in my_string if i.lower() != "c")
     return new_string
