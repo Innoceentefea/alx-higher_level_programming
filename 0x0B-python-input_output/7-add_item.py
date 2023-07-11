3c379a923ee6708b4bb3b63f6a01a6799d5578bd
INNOXSE <innocentefeakpoyibo@gmail.com>
2023-07-11 14:09:05 +0100

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Add all args to a Python list and save them to a JSON file"""
-
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
