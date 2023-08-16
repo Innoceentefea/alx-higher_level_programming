57710ec79fb3c4df9fb92cd5345138988811eb04
Innocentefea <innocentefeakpoyibo@gmail.com>
2023-08-16 18:18:22 +0100

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,11 +3,10 @@
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
 -- You can use only one SELECT statement
-SELECT tv_genres.name
-FROM tv_genres
-     JOIN tv_show_genres
-     	  ON tv_genres.id = tv_show_genres.genre_id
-     JOIN tv_shows
-     	  ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = "Dexter"
-ORDER BY tv_genres.name;
+SELECT title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Dexter'
+GROUP BY title
+ORDER BY title ASC;
