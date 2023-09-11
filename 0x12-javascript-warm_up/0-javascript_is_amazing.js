b363547b00bc576033911f82ed5cc2df75e9f71c
Innocentefea <innocentefeakpoyibo@gmail.com>
2023-09-11 18:34:17 +0100

    0x12-javascript-warm up

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -3,5 +3,5 @@
 // Write a script that prints “JavaScript is amazing”:
 // You must create a constant variable called myVar with the value “JavaScript is amazing”
 
-const myVar = 'JavaScript is amazing';
+const myVar= 'JavaScript is amazing';
 console.log(myVar);
