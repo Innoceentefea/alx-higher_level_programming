4bd3fba8b98a0374ede948056085d1980cba073d
Innocentefea <innocentefeakpoyibo@gmail.com>
2023-09-11 16:21:58 +0100

    0x12-javascript-warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,6 @@
-#!/user/bin/env node
-
-// Write a script that prints a message depending of the number of arguments passed:
-// If no arguments are passed to the script, print “No argument”
-// If only one argument is passed to the script, print “Argument found”
-// Otherwise, print “Arguments found”
+#!/user/bin/node
 
 const argsCount = process.argv.length - 2;
-
 if (argsCount === 0) {
   console.log('No argument');
 } else if (argsCount === 1) {
