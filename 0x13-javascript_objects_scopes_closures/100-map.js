82d3f3cc5c78917701effadfcee901e3e2d9aac0
Innocentefea <innocentefeakpoyibo@gmail.com>
2023-09-13 19:11:28 +0100

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,6 +2,4 @@
 const list = require('./100-data.js').list;
 
 console.log(list);
-console.log(list.map(function (x, idx) {
-  return (x * idx);
-}));
+console.log(list.map( (x, idx) => (x * idx)));
