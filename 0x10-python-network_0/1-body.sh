4760c5e8f655db9b253d8e508b318fab87feb9c2
Innocentefea <innocentefeakpoyibo@gmail.com>
2023-09-28 06:44:50 -0700

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#Write a Bash script that takes in a URL, sends a GET request to the URL,
-#and displays the body of the response
-#Display only body of a 200 status code response
-curl -sL "$1"
+# A bash Script that displays only body of response
+curl -sfL "$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Write a Bash script that sends a DELETE request to the URL 
-#passed as the first argument and displays the body of the response
-curl -sX DELETE "$1"
+# Bash that Sends a DELETE request and display the response body.
+curl -s "$1" -X DELETE
